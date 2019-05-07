6c3b2d8be0fe98d7cb2c3100f4e11472540efa6c
qingmei2 <mq2553299@gmail.com>
2019-05-07 19:01:59 +0800

    init project.

---
+++ Data-Structure/Data-Structure/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Data-Structure
+//
+//  Created by qingmei2 on 2019/5/7.
+//  Copyright © 2019 qingmei2. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
